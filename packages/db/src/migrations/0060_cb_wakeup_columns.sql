b4a13041a25c5568d1335a3c6ce6632ed444f09b
ClippyCTO <clippycto@clippyclaw.local>
2026-04-21 12:37:16 -0700

    Revert "feat(CLI-162): add integration-contract exports to circuit-breaker + schema migration"
    This reverts commit f2d94f90247ec0382cdd9e70d53fc012ed7011e9.

--- packages/db/src/migrations/0060_cb_wakeup_columns.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "agent_wakeup_requests" ADD COLUMN "issue_id" uuid REFERENCES "issues"("id");
---> statement-breakpoint
-ALTER TABLE "agent_wakeup_requests" ADD COLUMN "scheduled_at" timestamp with time zone;
